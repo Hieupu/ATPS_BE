a70c4c49d5c1b344d3b67770a5a291c18ab4f412
NguyenPTHE172112 <nguyenpthe172112@fpt.edu.vn>
2025-10-16 14:30:25 +0700

    done backend register, with Google, Facebook

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
-const router = require("./routes/router");
+const router = require("./routes/routerAuth");
+const passport = require("passport");
 
 dotenv.config();
 const app = express();
+
 app.use(express.json());
+app.use(passport.initialize());
 app.use("/api", router);
 
-const PORT = process.env.PORT || 8080;
-
+const PORT = process.env.PORT || 9999; 
 connectDB().then(() => {
-  app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+    app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
 });
