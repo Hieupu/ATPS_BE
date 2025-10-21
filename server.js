2c6d8b10c0a5425026985d10b25f50f47e527703
phatmvp <phat0972430986@gmail.com>
2025-10-21 22:46:27 +0700

    g

--- server.js
+++ server.js
@@ -9,13 +9,12 @@ const cors = require("cors");
 dotenv.config();
 const app = express();
 
-
 const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:3000";
 const corsOptions = {
-  origin: FRONTEND_URL,                 
+  origin: FRONTEND_URL,
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true,                   
+  credentials: true,
 };
 
 app.use(cors(corsOptions));
@@ -23,11 +22,10 @@ app.options(/.*/, cors(corsOptions));
 app.use(express.json());
 app.use(passport.initialize());
 
-
 app.use("/api", router);
 app.use("/api/profile", profileRoutes);
 
-const PORT = process.env.PORT || 9999; 
+const PORT = process.env.PORT || 9999;
 connectDB().then(() => {
   app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
 });
