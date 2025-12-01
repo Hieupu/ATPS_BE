33fd5bc7ca6560b9fff7c99185c66b9a7fcc4acf
S1nsoledad <hieupu2003@gmail.com>
2025-12-02 00:23:09 +0700

    update auto attendance

--- controllers/zoomController.js
+++ controllers/zoomController.js
@@ -1,4 +1,29 @@
 const crypto = require("crypto");
+const axios = require("axios")
+const attendanceService = require("../services/attendanceService");
+const { use } = require("passport");
+const { start } = require("repl");
+const { time } = require("console");
+
+let zoomCache = [];
+
+const cleanupZoomCache = () => {
+  const now = Date.now();
+  const TWO_HOURS = 140 * 60 * 1000;
+
+  const beforeCount = zoomCache.length;
+
+  zoomCache = zoomCache.filter(item => {
+    return (now - item.cachedAt) < TWO_HOURS;
+  });
+
+  if (beforeCount !== zoomCache.length) {
+    console.log(`Đã dọn ${beforeCount - zoomCache.length} bản ghi cũ khỏi zoomCache. Còn lại: ${zoomCache.length}`);
+  }
+};
+
+setInterval(cleanupZoomCache, 15 * 60 * 1000);
+cleanupZoomCache();
 
 class ZoomController {
   // POST /api/zoom/signature
@@ -36,7 +61,6 @@ class ZoomController {
       };
 
       const base64url = (source) => {
-        // Encode string → base64url
         let encoded = Buffer.from(JSON.stringify(source))
           .toString("base64")
           .replace(/=/g, "")
@@ -69,6 +93,101 @@ class ZoomController {
       res.status(500).json({ error: "Failed to generate signature" });
     }
   }
+
+  // POST /api/zoom/webhook
+  async zoomRes(req, res) {
+  console.log("========= 🔔 ZOOM WEBHOOK RECEIVED 🔔 =========");
+  console.log("Full body:", JSON.stringify(req.body, null, 2));
+  if (req.body.accId && req.body.sessionId) {
+
+  const existing = zoomCache.findIndex(z => z.sessionId === req.body.sessionId && z.userName === req.body.userName);
+
+  const newEntry = {
+    accId: req.body.accId,
+    sessionId: req.body.sessionId,
+    userName: req.body.userName,
+    startTime: req.body.startTime,
+    endTime: req.body.endTime,
+    date: req.body.date,
+    cachedAt: Date.now()
+  };
+
+  if (existing !== -1) {
+    zoomCache[existing] = newEntry;
+  } else {
+    zoomCache.push(newEntry);
+  }
+}
+  try {
+    const event = req.body.event;
+    const data  = req.body.payload?.object;
+
+    // URL VALIDATION
+    if (event === "endpoint.url_validation") {
+
+      const plainToken = req.body.payload.plainToken;
+      const webhookSecret = process.env.ZOOM_WEBHOOK_SECRET;
+
+      const hash = crypto
+        .createHmac("sha256", webhookSecret)
+        .update(plainToken)
+        .digest("hex");
+
+      return res.json({
+        plainToken,
+        encryptedToken: hash,
+      });
+    }
+
+    // PARTICIPANT JOINED
+    if (event === "meeting.participant_joined") {
+
+      const tokenUrl = `https://zoom.us/oauth/token?grant_type=account_credentials&account_id=${process.env.ZOOM_ACCOUNT_ID}`;
+    const authHeader = Buffer.from(`${process.env.ZOOM_CLIENT_ID}:${process.env.ZOOM_CLIENT_SECRET}`).toString("base64");
+
+    const response = await axios.post(tokenUrl, null, {
+      headers: { Authorization: `Basic ${authHeader}` },
+    });
+    console.log( response.data.access_token);
+
+
+      const userName   = data.participant?.user_name;
+      const joinTime   = data.participant?.join_time;
+
+      const parseData = zoomCache.find(z => z.userName === userName);
+
+      const attend = await attendanceService.attendanceLogic(parseData?.accId, parseData?.startTime, parseData?.endTime,      
+      parseData?.date, parseData?.sessionId, joinTime, "join");
+      console.log("📌 Attendance result:", attend);
+
+      return res.status(200).json({ success: true, join: attend });
+    }
+
+    // PARTICIPANT LEFT
+    if (event === "meeting.participant_left") {
+
+      const userName   = data.participant?.user_name;
+      const leaveTime  = data.participant?.leave_time;
+
+      const parseData = zoomCache.find(z => z.userName === userName);
+      const attend = await attendanceService.attendanceLogic(parseData?.accId, parseData?.startTime, parseData?.endTime,      
+      parseData?.date, parseData?.sessionId, leaveTime, "leave");
+
+      console.log("Attendance result:", attend);
+
+      return res.status(200).json({ success: true, leave: attend });
+    }
+
+    // DEFAULT – ignored events
+    console.log("Event ignored:", event);
+    res.status(200).send("ignored");
+
+  } catch (error) {
+    console.error("Auto attendance error:", error?.response?.data || error);
+    res.status(500).json({ message: "Failed to take attendance" });
+  }
+}
+
 }
 
 module.exports = new ZoomController();
