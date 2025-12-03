a1fca8937e058eb5a4291b0ea7e0c72459b8df78
NguyenPTHE172112 <nguyenpthe172112@fpt.edu.vn>
2025-12-03 18:10:25 +0700

    update instructorExam funtion autoUpdateExamStatus

--- server.js
+++ server.js
@@ -20,7 +20,8 @@ const cors = require("cors");
 const instructorCourseRoutes = require("./routes/instructorCourseRouter");
 const instructorClassRoutes = require("./routes/instructorClassRoutes");
 const instructorExamRoutes = require("./routes/instructorExamRoutes");
-
+const instructorExamRepository = require("./repositories/instructorExamRepository");
+process.env.TZ = 'Asia/Ho_Chi_Minh';
 dotenv.config();
 const app = express();
 
@@ -73,5 +74,8 @@ app.use("/api/zoom", zoomRoutes);
 
 const PORT = process.env.PORT || 9999;
 connectDB().then(() => {
-  app.listen(PORT, () => console.log(`@ Server running on port ${PORT}`));
+  instructorExamRepository.autoUpdateExamStatus();
+  app.listen(PORT, () => {
+    console.log(`@ Server running on port ${PORT}`);
+  });
 });
